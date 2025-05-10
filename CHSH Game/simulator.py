9e46637bc08932d343d303ce480915a987961c68
Ebrahim Elgazar <ebrahimjrr@gmail.com>
2025-05-10 19:30:23 +0200

    CHSH Multi Qubit

---
+++ CHSH Game/simulator.py
@@ -0,0 +1,36 @@
+import numpy as np
+from interface import Qubit
+from abc import ABCMeta, abstractmethod
+from interface import Qubit, QuantumDevice
+
+KET_0 = np.array([[1] , [0]], dtype= 'complex')
+X = np.array([[0,1] , [1, 0]], dtype= 'complex') / np.sqrt(2)
+H = np.array([[1,1], [1,-1]], dtype= 'complex') / np.sqrt(2)
+
+class SimulateQubit(Qubit):
+    def __init__(self):
+        self.reset()
+    
+    def h(self):
+        self.state = H @ self.state
+
+    def x(self):
+        self.state  = X @ self.state
+
+    def measure(self) -> bool:
+        pr0 = np.abs(self.state[0, 0]) ** 2
+        sample = np.random.random() <= pr0
+        return bool(0 if sample else 1)
+    
+    def reset(self):
+        self.state = KET_0.copy()
+
+class SingleQubitSimulator(QuantumDevice):
+    available_qubits = [SimulateQubit()]
+
+    def allocate_qubit(self) -> SimulateQubit:
+        if self.available_qubits:
+            return self.available_qubits.pop()
+
+    def deallocate_qubit(self, qubit: SimulateQubit):
+        self.available_qubits.append(qubit)
